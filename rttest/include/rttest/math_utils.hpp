d9365db2b51ce2719575a9e56ad34ab7b4fa4824
Alejandro Hernández Cordero <ahcorde@gmail.com>
2026-06-23 09:24:18 +0200

    constrain calculate_stddev with std::ranges::input_range + std::is_arithmetic_v (#150)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- rttest/include/rttest/math_utils.hpp
+++ rttest/include/rttest/math_utils.hpp
@@ -18,10 +18,13 @@
 #include <algorithm>
 #include <cmath>
 #include <numeric>
+#include <ranges>  // NOLINT
+#include <type_traits>
 #include <vector>
 
-template<typename container>
-double calculate_stddev(const container & vec)
+template<std::ranges::input_range Container>
+requires std::is_arithmetic_v<std::ranges::range_value_t<Container>>
+double calculate_stddev(const Container & vec)
 {
   double n = vec.size();
   double mean = std::accumulate(
